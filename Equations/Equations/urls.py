d26cba0ffc2a94b1dce2cc2c71ce5b77aa13c254
Bradley Middleton <bmmidd227@gmail.com>
2025-10-23 09:26:07 -0500

    should have added a page to check health of website

--- Equations/Equations/urls.py
+++ Equations/Equations/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Calculator/",  include("Calculator.urls"))
+    path("health/", lambda request: HttpResponse("OK", status=200)),
 ]
