42939d9f02ff6cd6efdd49b99c9abbb13f2fde6d
Bradley Middleton <bmmidd227@gmail.com>
2025-10-27 20:53:19 -0500

    Renamed a bunch of hmtl files, added infrastructure for future urls, added favicon to homepage

--- Equations/Calculator/urls.py
+++ Equations/Calculator/urls.py
@@ -2,9 +2,18 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("engineering/", views.engineering, name="engineeringCalculator"),
-    path("science/", views.science, name="scienceCalculator"),
-    path("math/", views.math, name="mathCalculator"),
-    path("general/", views.general, name="generalCalculator"),
+    path("", views.homePage, name="HomePage"),
+    #path("Engineering/", views.engineering, name="engineeringHomePage"),
+    #path("Engineering/Intro-to-Engineering-Design", views.IED, name="introToEngineeringDesignCalculator"),
+    path("Engineering/Principles-of-Engineering/", views.POE, name="principlesOfEngineeringCalculator"),
+    #path("Science/", views.science, name="scienceHomePage"),
+    #path("Science/Chemistry/", views.chemistry, name="chemistryHomePage"),
+    #path("Science/Chemistry/Chemisty-I", views.chemistryI, name="chemistryICalculator"),
+    #path("Science/Chemistry/Chemisty-I-Honors", views.chemistryIHonors, name="chemistryIHonorsCalculator"),
+    path("Science/Chemistry/ACP-Chemistry-I-Honors/", views.chemistryIACP, name="chemistryIACPCalculator"),
+    #path("Math/", views.math, name="mathHomePage"),
+    #path("Math/Algebra", views.algera, name="algebraHomePage"),
+    #path("Math/Algebra/Algebra-II/", views.algebraII, name="algebraIICalculator"),
+    path("Math/Algebra/Algebra-II-Honors/", views.algebraIIHonors, name="algebraIIHonorsCalculator"),
+    path("General/", views.general, name="generalCalculator"),
 ]
--- Equations/Calculator/views.py
+++ Equations/Calculator/views.py
@@ -2,17 +2,44 @@ from django.http import HttpResponse # type: ignore
 from django.shortcuts import render # type: ignore
 
 # Create your views here.
-def index(request):
+def homePage(request):
     return render(request, "CodeForEachWebpage/HomeScreen.html")
 
-def engineering(request):
-    return render(request, "CodeForEachWebpage/EngineeringCalculator.html")
+#def engineering(request):
+    #return render(request, "CodeForEachWebpage/EngineeringHomePage.html")
 
-def science(request):
-    return render(request, "CodeForEachWebpage/ScienceCalculator.html")
+#def IED(request):
+    #return render(request, "CodeForEachWebpage/IEDCalculator.html")
 
-def math(request):
-    return render(request, "CodeForEachWebpage/MathCalculator.html")
+def POE(request):
+    return render(request, "CodeForEachWebpage/POECalculator.html")
+
+#def science(request):
+    #return render(request, "CodeForEachWebpage/ScienceHomePage.html")
+
+#def chemistry(request):
+    #return render(request, "CodeForEachWebpage/ChemistryHomePage.html")
+
+#def chemistryI(request):
+    #return render(request, "CodeForEachWebpage/ChemistryICalculator.html")
+
+#def chemistryIHonors(request):
+    #return render(request, "CodeForEachWebpage/ChemistryIHonorsCalculator.html")
+
+def chemistryIACP(request):
+    return render(request, "CodeForEachWebpage/ChemistryICalculator.html")
+
+#def math(request):
+    #return render(request, "CodeForEachWebpage/MathHomePage.html")
+
+#def algera(request):
+    #return render(request, "CodeForEachWebpage/AlgebraHomePage.html")
+
+#def algebraII(request):
+    #return render(request, "CodeForEachWebpage/AlgebraIICalculator.html")
+
+def algebraIIHonors(request):
+    return render(request, "CodeForEachWebpage/AlgebraIICalculator.html")
 
 def general(request):
     return render(request, "CodeForEachWebpage/GeneralCalculator.html")
